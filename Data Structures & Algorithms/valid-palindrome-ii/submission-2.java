181a64b74026ebb8b2276d75fb0c095b73d25563
codefellas0-wq <codefellas0@gmail.com>
2026-04-18 12:56:57 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, h = s.length() - 1;
+
+        while (l < h) {
+            if (s.charAt(l) != s.charAt(h)) {
+                return isPalindrome(s, l + 1, h) || isPalindrome(s, l, h - 1);
+            }
+            l++;
+            h--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int h) {
+        while (l < h) {
+            if (s.charAt(l) != s.charAt(h)) return false;
+            l++;
+            h--;
+        }
+        return true;
+    }
+}
