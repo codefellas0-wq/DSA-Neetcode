ae59b5b6548def3a94f49c0c79919c2f53b8d445
codefellas0-wq <codefellas0@gmail.com>
2026-04-24 23:16:29 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0 , buy = prices[0];
+        int prof = 0;
+        for(int i = 0; i<prices.length; i++){
+            if(buy>prices[i]){
+                buy = prices[i];
+            }
+            prof = prices[i] - buy;
+            max = Math.max(prof , max);
+        }
+        return max;
+    }
+}
